a4ea1de3ab7d1529afcb20e65e650f243fbac5ca
DanielSurf10 <71985055+DanielSurf10@users.noreply.github.com>
2024-06-11 13:24:49 -0300

    algumas coisas

--- directory philo/include/philosophers.h
+++ directory philo/include/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: danbarbo <danbarbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 22:12:55 by danbarbo          #+#    #+#             */
-/*   Updated: 2024/06/08 23:27:28 by danbarbo         ###   ########.fr       */
+/*   Updated: 2024/06/11 11:55:16 by danbarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,22 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <sys/time.h>
+
+typedef struct timeval t_timeval;
 
 typedef struct s_philo
 {
 	int				id;
-	// int				left_fork;
-	// int				right_fork;
-	// int				times_to_eat;
-	// int				eat_count;
-	// long long		last_eat;
-	// pthread_t		thread;
-	// pthread_mutex_t	*forks;
+	int				*left_fork;			// Ponteiro pra um fork do array de forks
+	int				*right_fork;
 	pthread_mutex_t	*print;
-	// pthread_mutex_t	*death;
+	struct timeval	start;
 }	t_philo;
+
+typedef struct s_waiter
+{
+	int		*forks;		// Array de forks
+}	t_waiter;
 
 #endif
--- directory philo/src/main.c
+++ directory philo/src/main.c
@@ -6,38 +6,89 @@
 /*   By: danbarbo <danbarbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 22:19:55 by danbarbo          #+#    #+#             */
-/*   Updated: 2024/06/08 23:44:53 by danbarbo         ###   ########.fr       */
+/*   Updated: 2024/06/11 12:51:00 by danbarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void *philosopher(void *arg)
+void	print_ms(t_timeval now, t_timeval start)
 {
-	t_philo	philo;
+	long int	microseconds_now;
+	long int	microseconds_since_start;
+
+	microseconds_now = (now.tv_sec * 1000000) + now.tv_usec;
+	microseconds_since_start = microseconds_now - ((start.tv_sec * 1000000) + start.tv_usec);
+	printf("%li ", microseconds_since_start);
+}
+
+void	*philosopher(void *arg)
+{
+	long int		microseconds_since_start;
+	long int		microseconds_now;
+	t_philo			philo;
+	t_timeval		now;
+	t_timeval		start;
 
 	philo = *(t_philo *) arg;
+
+	gettimeofday(&now, NULL);
+	start = philo.start;
+
 	pthread_mutex_lock(philo.print);
-	printf("Philosopher %i is eating\n", philo.id);
-	printf("Philosopher %i is sleeping\n", philo.id);
-	printf("Philosopher %i is thinking\n", philo.id);
+	print_ms(now, start);
+	printf("Philosopher %i is eating\n", microseconds_since_start, philo.id);
 	pthread_mutex_unlock(philo.print);
+
+// 	gettimeofday(&time, NULL);
+// 	microseconds_now = (time.tv_sec * 1000000) + (time.tv_usec);
+// 	microseconds_since_start = microseconds_now - ((philo.start.tv_sec * 1000000) + (philo.start.tv_usec));
+// 	pthread_mutex_lock(philo.print);
+// 	print_ms();
+// 	printf("%8li Philosopher %i is eating\n", microseconds_since_start, philo.id);
+// 	// pthread_mutex_unlock(philo.print);
+// 	usleep(1000000);
+//
+// 	gettimeofday(&time, NULL);
+// 	microseconds_now = (time.tv_sec * 1000000) + (time.tv_usec);
+// 	microseconds_since_start = microseconds_now - ((philo.start.tv_sec * 1000000) + (philo.start.tv_usec));
+// 	// pthread_mutex_lock(philo.print);
+// 	print_ms();
+// 	printf("%8li Philosopher %i is sleeping\n", microseconds_since_start, philo.id);
+// 	// pthread_mutex_unlock(philo.print);
+// 	usleep(2000000);
+//
+// 	gettimeofday(&time, NULL);
+// 	microseconds_now = (time.tv_sec * 1000000) + (time.tv_usec);
+// 	microseconds_since_start = microseconds_now - ((philo.start.tv_sec * 1000000) + (philo.start.tv_usec));
+// 	// pthread_mutex_lock(philo.print);
+// 	print_ms();
+// 	printf("%8li Philosopher %i is thinking\n", microseconds_since_start, philo.id);
+// 	pthread_mutex_unlock(philo.print);
 	return (NULL);
 }
 
 int	main(int argc, char *argv[])
 {
-	int				philos_count = 5;
+	int				philos_count = 2;
+	t_timeval		start;
 	t_philo			philos[philos_count];
 	pthread_t		thread[philos_count];
+	t_waiter		waiter;
 	pthread_mutex_t	print_mutex;
 
 	pthread_mutex_init(&print_mutex, NULL);
+	gettimeofday(&start, NULL);
+
+	waiter.forks = malloc(sizeof(int) * philos_count);
 
 	for (int i = 0; i < philos_count; i++)
 	{
 		philos[i].id = i + 1;
 		philos[i].print = &print_mutex;
+		philos[i].start = start;
+		philos[i].left_fork = &waiter.forks[(i + philos_count - 1) % philos_count];
+		philos[i].right_fork = i;
 		pthread_create(&thread[i], NULL, philosopher, &philos[i]);
 	}
 
