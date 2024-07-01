5c443b30498cfd0d1c5d0a8821cf6c4376a189c5
DanielSurf10 <daniel101surf@gmail.com>
2024-07-01 16:34:16 -0300

    começando o bonus

---
+++ philo_bonus/main.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danbarbo <danbarbo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/08 22:19:55 by danbarbo          #+#    #+#             */
+/*   Updated: 2024/07/01 16:32:49 by danbarbo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+// ./philo <philo_number> <time_to_die> <time_to_eat> <time_to_sleep> [max_eat]
+
+t_main_data	set_default_data()
+{
+	t_main_data	data;
+
+	data.philos_count = 4;
+	data.time_to_die = 800;
+	data.time_to_eat = 200;
+	data.time_to_sleep = 200;
+	data.max_eat_count = 5;
+	data.is_valid = 1;
+}
+
+t_main_data	validate_args(int argc, char *argv[])
+{
+	t_main_data	data;
+
+	if (argc == 1)
+		data = set_default_data();
+	else if (!(argc >= 5 && argc <= 6))
+	{
+		write(2, "Invalid argument number!\n", 25);
+		exit(1);
+	}
+	data = get_data(argv);
+	if (data.is_valid == 0)
+	{
+		write(2, "Invalid arguments!\n", 19);
+		exit(1);
+	}
+	return (data);
+}
+
+void	philos_init(t_main_data	*data)
+{
+	int	i;
+
+	data->philos = malloc(sizeof(t_philo) * data->philos_count);
+	memset(data->philos, 0, sizeof(t_philo) * data->philos_count);
+
+	i = 0;
+	while (i < data->philos_count)
+	{
+		data->philos[i].id = i;
+		data->philos[i].time_to_die = data->time_to_die;
+		data->philos[i].time_to_eat = data->time_to_eat;
+		data->philos[i].time_to_sleep = data->time_to_sleep;
+		data->philos[i].max_eat_count = data->max_eat_count;
+		data->philos[i].someone_died = &data->someone_died;
+		data->philos[i].start = data->start;
+		data->philos[i].last_eat = data->start;
+		data->philos[i].print = &data->print_mutex;
+		data->philos[i].left_fork_mutex = &data->forks_mutex[(i + data->philos_count - 1) % data->philos_count];
+		data->philos[i].right_fork_mutex = &data->forks_mutex[i];
+		data->philos[i].someone_died_mutex = &data->someone_died_mutex;
+		data->philos[i].philos_count = data->philos_count;
+		pthread_create(&data->philos[i].thread, NULL, philosopher, &data->philos[i]);
+		usleep(500);
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int			i;
+	t_main_data	data;
+
+	data = validate_args(argc, argv);
+	philos_init(&data);
+
+	wait_until_someone_finish_or_die(&data);
+
+	i = 0;
+	while (i < data.philos_count)
+	{
+		pthread_join(data.philos[i].thread, NULL);
+		i++;
+	}
+
+	pthread_mutex_destroy(&data.print_mutex);
+	pthread_mutex_destroy(&data.someone_died_mutex);
+
+	i = 0;
+	while (i < data.philos_count)
+	{
+		pthread_mutex_destroy(&data.forks_mutex[i]);
+		pthread_mutex_destroy(&data.philos[i].philo_mutex);
+		i++;
+	}
+
+	free(data.forks_mutex);
+	free(data.philos);
+
+	return (0);
+}
---
+++ philo_bonus/philosophers.h
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danbarbo <danbarbo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/08 22:12:55 by danbarbo          #+#    #+#             */
+/*   Updated: 2024/07/01 16:33:33 by danbarbo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+
+# define RST	"\001\033[0m\002"
+# define ORANGE	"\001\033[38;5;208m\002"
+# define BLUE	"\001\033[38;5;27m\002"
+# define GREEN	"\001\033[38;5;46m\002"
+# define RED	"\001\033[38;5;196m\002"
+# define YELLOW	"\001\033[38;5;226m\002"
+# define CYAN	"\001\033[38;5;51m\002"
+# define PURPLE	"\001\033[38;5;129m\002"
+# define GREY	"\001\033[38;5;240m\002"
+# define WHITE	"\001\033[38;5;231m\002"
+# define BLACK	"\001\033[38;5;16m\002"
+# define BOLD	"\001\033[1m\002"
+# define UNDER	"\001\033[4m\002"
+# define BLINK	"\001\033[5m\002"
+# define RST	"\001\033[0m\002"
+
+// # define ORANGE	""
+// # define BLUE	""
+// # define GREEN	""
+// # define RED	""
+// # define YELLOW	""
+// # define CYAN	""
+// # define PURPLE	""
+// # define GREY	""
+// # define WHITE	""
+// # define BLACK	""
+// # define BOLD	""
+// # define UNDER	""
+// # define BLINK	""
+// # define RST	""
+
+enum e_status
+{
+	GET_FORK,
+	EATING,
+	THINK,
+	SLEEP,
+};
+
+typedef struct timeval	t_timeval;
+
+typedef struct s_philo
+{
+	int				id;
+	int				philos_count;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				max_eat_count;
+	int				eat_count;
+	int				*someone_died;
+	t_timeval		start;
+	t_timeval		last_eat;
+	pthread_t		thread;
+	pthread_mutex_t	philo_mutex;
+	pthread_mutex_t	*print;
+	pthread_mutex_t	*left_fork_mutex;
+	pthread_mutex_t	*right_fork_mutex;
+	pthread_mutex_t	*someone_died_mutex;
+}	t_philo;
+
+typedef struct s_main_data
+{
+	int				is_valid;
+	int				philos_count;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				max_eat_count;
+	int				someone_died;
+	t_philo			*philos;
+	pthread_mutex_t	*forks_mutex;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	someone_died_mutex;
+	t_timeval		start;
+}	t_main_data;
+
+// philospher function
+void		*philosopher(void *arg);
+
+// philosophers actions
+void		get_forks(t_philo *philo);
+void		eat(t_philo *philo);
+void		philo_sleep(t_philo *philo);
+void		think(t_philo *philo);
+
+// die logic
+void		wait_until_someone_finish_or_die(t_main_data *data);
+
+// validation
+t_main_data	get_data(char **argv);
+
+// utils
+void		print_ms(t_timeval now, t_timeval start);
+int			ternary(int condition, int if_true, int if_false);
+void		print_color(char *color);
+
+// print mutex
+void		print_mutex(t_philo *philo, int status);
+void		print_mutex_get_fork(t_philo *philo);
+void		print_mutex_eating(t_philo *philo);
+void		print_mutex_sleep(t_philo *philo);
+void		print_mutex_think(t_philo *philo);
+
+#endif
